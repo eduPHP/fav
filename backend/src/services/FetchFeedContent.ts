ba7b3694a850c11f26897aad0bdf7c780f8ee217
Eduardo <edu@rdo.blog.br>
2021-04-24 18:04:00 -0300

    Handle cache on the frontend

--- backend/src/services/FetchFeedContent.ts
+++ backend/src/services/FetchFeedContent.ts
@@ -1,30 +1,10 @@
-import axios from 'axios'
-import Feed from '../models/Feed'
-import ParseXmlFeedContent, { FeedListInterface } from './ParseXmlFeedContent'
-import Cache from '../util/cache'
-
-const cache = Cache('file')
+import axios from 'axios';
+import Feed from '../models/Feed';
+import ParseXmlFeedContent, { FeedListInterface } from './ParseXmlFeedContent';
 
 export default class {
   async handle(feed: Feed): Promise<FeedListInterface> {
-    const key = `feed-list-${feed.id}`
-
-    const cachedContent = await cache.get<FeedListInterface>(key, null)
-
-    if (cachedContent) {
-      axios.get(feed.url).then(async response => {
-        const content = await ParseXmlFeedContent.parse(response.data)
-        await cache.put(key, content, 1)
-      })
-
-      return cachedContent
-    }
-
-    return axios.get(feed.url).then(async response => {
-      const parsedContent = await ParseXmlFeedContent.parse(response.data)
-      await cache.put(key, parsedContent, 1)
-
-      return parsedContent
-    })
+    const response = await axios.get(feed.url)
+    return await ParseXmlFeedContent.parse(response.data);
   }
 }
