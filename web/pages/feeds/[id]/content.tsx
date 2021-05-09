b5e37cc360242aa6f87f76dc58bdb32fd19b2115
Eduardo <edu@rdo.blog.br>
2021-05-09 00:00:14 -0300

    Add public feed provider selection

--- web/pages/feeds/[id]/content.tsx
+++ web/pages/feeds/[id]/content.tsx
@@ -1,4 +1,4 @@
-import { authenticated } from '../../../hooks/auth';
+import { authenticated, useAuth } from '../../../hooks/auth';
 import { FeedItem } from '../../index';
 import api from '../../../services/api';
 import { formatter } from '../../../util/dateFormatter';
@@ -7,6 +7,7 @@ import FeedsList from '../../../components/FeedsList';
 import Head from 'next/head';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useToast } from '../../../hooks/toasts';
 
 interface ProviderDetailsResponse {
   item: FeedItem[];
@@ -24,26 +25,46 @@ const Content = () => {
   const [provider, setProvider] = useState<ProviderDetails>(null);
   const [loading, setLoading] = useState(true);
   const router = useRouter();
+  const { addToast } = useToast();
+  const { authenticated } = useAuth();
   const id = router.query.id;
 
   async function getFeeds() {
-    const response = await api.get<ProviderDetailsResponse>(
-      `/feeds/contents/${id}`,
-    );
-    setProvider({
-      title: response.data.title,
-      link: response.data.link,
-      feeds: response.data.item.map(i => {
-        let domain = new URL(i.link);
-        return {
-          ...i,
-          site: domain.hostname,
-          pubDate: formatter.format(new Date(i.pubDate)),
-        };
-      }),
-    });
-
-    setLoading(false);
+    api
+      .get<ProviderDetailsResponse>(
+        authenticated
+          ? `/feeds/contents/${id}`
+          : `/feeds/public-contents/${id}`,
+      )
+      .then(response => {
+        setProvider({
+          title: response.data.title,
+          link: response.data.link,
+          feeds: response.data.item.map(i => {
+            let domain = new URL(i.link);
+            return {
+              ...i,
+              site: domain.hostname,
+              pubDate: formatter.format(new Date(i.pubDate)),
+            };
+          }),
+        });
+        setLoading(false);
+      })
+      .catch(err => {
+        switch (err.response?.status) {
+          case 401:
+            router.push('/login');
+            break;
+          default:
+            addToast({
+              title: 'Not Found!',
+              type: 'error',
+              description: 'This feed provider is not found or not public.',
+            });
+            setTimeout(() => router.push('/'), 3000);
+        }
+      });
   }
 
   useEffect(() => {
@@ -52,12 +73,7 @@ const Content = () => {
         await getFeeds();
       }
     })();
-  }, [id]);
-
-  const handleRefresh = useCallback(async () => {
-    setLoading(true);
-    await getFeeds();
-  }, []);
+  }, [id, authenticated]);
 
   return (
     <>
@@ -89,7 +105,7 @@ const Content = () => {
   );
 };
 Content.getInitialProps = async ctx => {
-  const { token } = authenticated(ctx);
+  const { token } = authenticated(ctx, { redirect: false });
   return { token };
 };
 
