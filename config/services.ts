d2cd99d694fd7d0f43270fd2a8ea0ff34b174771
Edu <edu@rdo.blog.br>
2021-06-24 00:22:00 -0300

    Dont work this way

--- config/services.ts
+++ config/services.ts
@@ -1,11 +1,11 @@
-const api_url = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
-  : `${process.env.NEXT_PUBLIC_SITE_URL}/api`;
-const site_url = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : process.env.NEXT_PUBLIC_SITE_URL;
+// const api_url = process.env.NEXT_PUBLIC_VERCEL_URL
+//   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
+//   : `${process.env.NEXT_PUBLIC_SITE_URL}/api`;
+// const site_url = process.env.NEXT_PUBLIC_VERCEL_URL
+//   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+//   : process.env.NEXT_PUBLIC_SITE_URL;
 
 export default {
-  api_url,
-  site_url,
+  api_url: `${process.env.NEXT_PUBLIC_SITE_URL}/api`,
+  site_url: process.env.NEXT_PUBLIC_SITE_URL,
 };
